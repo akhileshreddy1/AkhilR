cf8832d403d0baa9765a69708dc48c3620ff0073
akhileshreddy1 <akhilesh441@gmail.com>
2025-04-08 15:50:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! 🚀');
+  res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
